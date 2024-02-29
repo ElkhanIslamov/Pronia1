011a83f2c95c80781f5c7f8d136acd92bf6f759a
Elkhan Islamov <elkhanmi@code.edu.az>
2024-02-29 19:47:19 +0400

    commit

--- WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ed9558ecff3c9b1f21f345c1fb2a32abfb0d27d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88a3fd8726572b8420f0cedc767a3203d0ec9100")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
